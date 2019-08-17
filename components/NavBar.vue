f52e108e5b4a58064699e3b62713f798ceac6d1a
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-08-17 16:38:47 +0300

    Setup NavBar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,19 +1,82 @@
 <template>
-  <div class="nav-bar">
-    NavBar
-  </div>
+  <nav class="navbar navbar-expand-lg">
+    <nuxt-link to="/" class="navbar-brand">
+      <img src="~/assets/images/logo.png" alt="Logo">
+    </nuxt-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon" />
+    </button>
+    <div id="navbarNavDropdown" class="collapse navbar-collapse">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <nuxt-link class="nav-link" to="/">
+            Home <span class="sr-only">(current)</span>
+          </nuxt-link>
+        </li>
+        <li class="nav-item dropdown">
+          <nuxt-link id="navbarDropdownMenuLink" class="nav-link dropdown-toggle" to="/" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Categories
+          </nuxt-link>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <nuxt-link class="dropdown-item" to="/">
+              Electronics
+            </nuxt-link>
+            <nuxt-link class="dropdown-item" to="/">
+              Fashion
+            </nuxt-link>
+            <nuxt-link class="dropdown-item" to="/">
+              Home &amp; Kitchen
+            </nuxt-link>
+            <nuxt-link class="dropdown-item" to="/">
+              For Kids
+            </nuxt-link>
+          </div>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" to="/">
+            Offers
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" to="/">
+            Trending
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" to="/">
+            Under $11
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" to="/">
+            Account
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
+    <form class="form-inline">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+        Search
+      </button>
+    </form>
+  </nav>
 </template>
 
 <script>
 export default {
   name: "NavBar",
-  data() {
-    return {
-      title: 'this'
-    }
-  },
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.navbar {
+  .navbar-brand img {
+    height: 50px;
+  }
+  .nav-link {
+    padding: 5px 20px;
+  }
+}
 </style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,18 @@
 <template>
   <div class="homepage">
-    <h1>Homepage</h1>
-    <div>{{ product.title }}</div>
-    <nuxt-link to="/about">
-      About
-    </nuxt-link>
-    <div class="icons">
-      <font-awesome-icon :icon="['fab', 'facebook']" />
-      <font-awesome-icon :icon="['fas', 'coffee']" />
-      <CssLoader />
-    </div>
+    <NavBar />
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex"
-import CssLoader from '../components/CssLoader'
+import NavBar from '../components/NavBar'
 
 export default {
   name: "Homepage",
 
   components: {
-    CssLoader
+    NavBar
   },
-
-  computed: {
-    ...mapGetters({
-      products: "products/products",
-      product: "products/product"
-    })
-  },
-
-  mounted() {
-    this.showWelcomeMessage()
-  },
-
-  methods: {
-
-  },
-
-  notifications: {
-    showWelcomeMessage: {
-      title: 'Page Mounted',
-      message: 'Testing notifications',
-      type: 'success',
-      timeout: 3000
-    }
-  }
 }
 </script>
 
